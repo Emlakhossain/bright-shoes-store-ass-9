3ef26eb76a4dd01d9fef152492e07773f813d001
imranahmed1990 <imranmarin1990@gmail.com>
2022-03-26 13:24:47 +0600

    nav bar commit done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import './Header.css';
+import logo from '../../img/logo (2).png';
 
 const Header = () => {
     return (
-        <div>
-            <h2>Hello World class shoes shop</h2>
-            <p>Inshallah i will start coding right now</p>
-        </div>
+        <nav className='header'>
+            <div>
+                <img src={logo} alt="" />
+            </div>
+            <div>
+                <a href="/home">Home</a>
+                <a href="/shope">Shope</a>
+                <a href="/inventory">Inventory</a>
+                <a href="/about us">About Us</a>
+            </div>
+        </nav>
     );
 };
 
